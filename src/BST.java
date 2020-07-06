cd010c3152ad4e6cc50c9b4f08b881748a85f03a
Bassel Kanaan <bassel.9100@gmail.com>
2020-07-06 15:49:58 +0200

    added features to BST

--- src/BST.java
+++ src/BST.java
@@ -1,2 +1,9 @@
-public class BST {
+import java.util.*;
+/**
+ * Binary Search Tree implementation
+ * @author basselkanaan, bassel.9100@gmail.com
+ **/
+
+public class BST <T extends Comparable<T>>{
+  
 }
