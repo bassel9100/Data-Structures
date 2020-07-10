b25d03e0d24b7239c3d73029f2c1581460442b06
Bassel Kanaan <bassel.9100@gmail.com>
2020-07-10 18:31:38 +0200

    added hashtable datastructure

---
+++ src/HashTable.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class HashTable {
+
+    String[] array;
+    int size;
+    int itemsInArray = 0;
+
+    public static void main(String[] args){
+        
+    }
+
+    
+}
