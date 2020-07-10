cb071573bb2b438d4b15867aa57cda7b0f8ce9a6
Bassel Kanaan <bassel.9100@gmail.com>
2020-07-10 19:03:14 +0200

    Simple changes

--- src/HashTable.java
+++ src/HashTable.java
@@ -7,7 +7,21 @@ public class HashTable {
     int itemsInArray = 0;
 
     public static void main(String[] args){
-        
+
+    }
+
+    // Very bad but simple hashfunction
+    public void hashFunc1(String[] fromArr, String[] toArr){
+        for(int i = 0; i < fromArr.length; i++){
+            String newElement = fromArray[i];
+            toArray[Integer.parseInt(newElement)] = newElement;
+        }
+    }
+
+    public HashTable(int size){
+        this.size = size;
+        array = new String[size];
+        Arrays.fill(array, "null");
     }
 
     
