4d14ffa297b04bb3f36086efd125e50cfcaf8ee8
Bassel Kanaan <bassel.9100@gmail.com>
2020-07-11 12:26:14 +0200

    Nothing important

--- src/HashTable.java
+++ src/HashTable.java
@@ -18,11 +18,22 @@ public class HashTable {
         }
     }
 
+    // Much more effective hashfunction using modulus
+    public void hashFunc2(String[] fromArr, String[] toArr){
+        for(int i = 0; i < fromArr.length; i++){
+            String newElement = fromArray[i];
+            int index = Integer.parseInt(newElement) % 99; // Create an index 2 store value by taking the modulus
+            while(toArr[index] != "null") {
+                ++index;
+                index %= size;
+            }
+            toArr[index] = newElement;
+        }
+    }
+
     public HashTable(int size){
         this.size = size;
         array = new String[size];
         Arrays.fill(array, "null");
     }
-
-    
 }
